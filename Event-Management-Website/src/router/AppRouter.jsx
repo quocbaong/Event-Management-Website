74ccb9fa53d09572ffdc136b838edb678100878b
QuocBao <nbao76296@gmail.com>
2026-04-21 14:12:05 +0700

    feat: UI Attend Dashboard

--- Event-Management-Website/src/router/AppRouter.jsx
+++ Event-Management-Website/src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from '../components/layouts/MainLayout';
+import AttendeeLayout from '../components/layouts/AttendeeLayout';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
 import DashboardPage from '../pages/DashboardPage';
 import LandingPage from '../pages/LandingPage';
 import EventsPage from '../pages/EventsPage';
 import EventDetailPage from '../pages/EventDetailPage';
+import AttendeeDashboardPage from '../pages/AttendeeDashboardPage';
 
 
 const AppRouter = () => {
@@ -21,13 +23,19 @@ const AppRouter = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         
-        {/* Protected Routes - With Sidebar/Header */}
+        {/* Organizer Protected Routes - With Sidebar/Header */}
         <Route element={<MainLayout />}>
           <Route path="/dashboard" element={<DashboardPage />} />
-          {/* Add other protected routes here */}
+          {/* Add other organizer routes here */}
         </Route>
 
-        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        {/* Attendee Protected Routes - Separate Layout */}
+        <Route element={<AttendeeLayout />}>
+          <Route path="/attendee/dashboard" element={<AttendeeDashboardPage />} />
+          {/* Add other attendee routes here: /attendee/tickets, /attendee/calendar, etc. */}
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
